ef2036df56a59684a2ae78c5102634fb84f818c3
msa0683 <msa0683@yahoo.com>
2017-08-17 18:50:45 -0700

    index.js file done

--- config/routes.js
+++ config/routes.js
@@ -1,83 +1,95 @@
-// Routes
-// ======
-ar express = require("express");
-var router = express.Router();
-
-var request = require("request");
-var cheerio = require("cheerio");
-
-var Article = require("../models/Article");
-var Note = require("../models/Note");
+// Server routes
 
 
-app.get("/", function(req, res) {
-  Article.find({}, function (err, doc) {
-    res.render('index', {articles: doc})
-  })
-});
+var scrape = require("../scripts/scrape");
 
-app.get("/scrape", function(req, res) {
-  request("https://www.nytimes.com", function(err, response, html) {
-    var $ = cheerio.load(html);
-    $("article h2").each(function(i, element) {
-      var result = {};
-      result.title = $(this).children("a").text();
-      result.link = $(this).children("a").attr("href");
-      result.saveState = false;
-      var entry = new Article(result);
-      entry.save(function(err, doc) {
-        if (err) {
-          console.log(err);
-        }
-        else {
-          console.log(doc);
-        }
+var headlinesController = require("../controllers/headlines");
+var notesController = require("../controllers/notes");
 
-      });
+module.exports = function(router) {
 
+  router.get("/", function(req, res) {
+    res.render("home");
+  });
+
+  router.get("/saved", function(req, res) {
+    res.render("saved");
+  });
+
+  router.get("/api/fetch", function(req, res) {
+
+    headlinesController.fetch(function(err, docs) {
+      if (!docs || docs.insertedCount === 0) {
+        res.json({
+          message: "No new articles today. Check back tomorrow!"
+        });
+      }
+      else {
+        res.json({
+          message: "Added " + docs.insertedCount + " new articles!"
+        });
+      }
     });
   });
-  res.send("Scrape Complete");
-});
 
-app.get("/saved", function(req, res) {
-  Article.find({saveState:true}, function(err, doc) {
-    if (err) throw err
-    else {
-      console.log(doc)
-      res.render("saved", {articles: doc});
+  router.get("/api/headlines", function(req, res) {
+    var query = {};
+    if (req.query.saved) {
+      query = req.query;
     }
+
+    headlinesController.get(query, function(data) {
+      res.json(data);
+    });
   });
-})
 
-app.get("/article/:id/save", function (req, res) {
-  Article.findOneAndUpdate({"_id": req.params.id}, {$set: {"saveState": true}}, function (err, doc) {
-    if (err) throw err
-    else {
-      res.redirect('/');
+  router.delete("/api/headlines/:id", function(req, res) {
+    var query = {};
+    query._id = req.params.id;
+
+    headlinesController.delete(query, function(err, data) {
+      res.json(data);
+    });
+  });
+
+  router.patch("/api/headlines", function(req, res) {
+
+
+    headlinesController.update(req.body, function(err, data) {
+
+      res.json(data);
+    });
+  });
+
+
+  router.get("/api/notes/:headline_id?", function(req, res) {
+    var query = {};
+    if (req.params.headline_id) {
+      query._id = req.params.headline_id;
     }
-  })
-})
 
-app.get("/article/:id/delete",function(req, res) {
-  Article.findOneAndUpdate({"_id": req.params.id}, {$set: {"saveState": false}}, function (err, doc) {
-    if(err) throw err
-    else {
-      res.redirect("/saved");
-    }
 
-  })
-})
+    notesController.get(query, function(err, data) {
+      res.json(data);
+    });
+  });
 
-app.post("/notes/:article_id", function(req, res) {
-  var newNote = new Note(req.body)
-  newNote.save(function(err, note) {
-    console.log(note)
-    if (err) throw err
-    else {
-      Article.findOneAndUpdate({"_id" : req.params.article_id}, {$set: {"note" : note}}, function (err, doc) {
-        res.redirect('/saved');
-      })
-    }
-  })
-})
+
+  router.delete("/api/notes/:id", function(req, res) {
+    var query = {};
+    query._id = req.params.id;
+
+    notesController.delete(query, function(err, data) {
+
+      res.json(data);
+    });
+  });
+
+
+  router.post("/api/notes", function(req, res) {
+    notesController.save(req.body, function(data) {
+
+      res.json(data);
+    });
+  });
+};
